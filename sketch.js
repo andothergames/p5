ac072e81808ef5a99ecff0abf9c84c354004e5c3
Joy <32204909+andothergames@users.noreply.github.com>
2024-04-05 22:14:25 +0100

    testing 2x3 grid

--- sketch.js
+++ sketch.js
@@ -1,70 +1,129 @@
 var myp5 = new p5(sketch);
 //variable sketch serves as template for what will run
 var sketch = function (p) {
+  p.setup = function () {
+    p.createCanvas(400, 400);
+  };
 
-p.setup = function () {
-  p.createCanvas(400, 400);
+  p.draw = function () {
+    p.noStroke();
+    p.background("#f0c7ee");
+
+    //eyes
+
+    p.fill("#000000");
+    p.ellipse(150, 180, 60);
+    p.ellipse(250, 180, 60);
+  };
+
+  function drawCircle(c, x, y, r) {
+    p.fill(c);
+    ellipse(x, y, r);
+  }
 };
-
-p.draw = function () {
-  p.noStroke();
-  p.background("#f0c7ee");
-
-  //eyes
-
-  p.fill("#000000");
-  p.ellipse(150, 180, 60)
-  p.ellipse(250, 180, 60)
-  
-}
-
-function drawCircle(c, x, y, r) {
-  p.fill(c);
-  ellipse(x, y, r);
-}
-
-}
-
 
 var myp5 = new p5(sketch);
 //variable sketch serves as template for what will run
 var sketch = function (p) {
+  p.setup = function () {
+    p.createCanvas(400, 400);
+  };
 
-p.setup = function () {
-  p.createCanvas(400, 400);
+  p.draw = function () {
+    p.noStroke();
+    p.background("#02f5f5");
+
+    //eyes
+
+    p.fill("#000000");
+    p.ellipse(150, 180, 60);
+    p.ellipse(250, 180, 60);
+  };
 };
-
-p.draw = function () {
-  p.noStroke();
-  p.background("#02f5f5");
-
-  //eyes
-
-  p.fill("#000000");
-  p.ellipse(150, 180, 60)
-  p.ellipse(250, 180, 60)
-}
-}
 
 var myp5 = new p5(sketch);
 //variable sketch serves as template for what will run
 var sketch = function (p) {
+  p.setup = function () {
+    p.createCanvas(400, 400);
+  };
 
-p.setup = function () {
-  p.createCanvas(400, 400);
+  p.draw = function () {
+    p.noStroke();
+    p.background("#000000");
+
+    //eyes
+
+    p.fill("#02f5f5");
+    p.ellipse(150, 180, 60);
+    p.fill("#f0c7ee");
+    p.ellipse(250, 180, 60);
+  };
 };
 
-p.draw = function () {
-  p.noStroke();
-  p.background("#000000");
-
-  //eyes
-
-  p.fill("#02f5f5");
-  p.ellipse(150, 180, 60)
-  p.fill("#f0c7ee")
-  p.ellipse(250, 180, 60)
-}
-}
+var myp5 = new p5(sketch);
 
 var myp5 = new p5(sketch);
+//variable sketch serves as template for what will run
+var sketch = function (p) {
+  p.setup = function () {
+    p.createCanvas(400, 400);
+  };
+
+  p.draw = function () {
+    p.noStroke();
+    p.background("#f0c7ee");
+
+    //eyes
+
+    p.fill("#000000");
+    p.ellipse(150, 180, 60);
+    p.ellipse(250, 180, 60);
+  };
+
+  function drawCircle(c, x, y, r) {
+    p.fill(c);
+    ellipse(x, y, r);
+  }
+};
+
+var myp5 = new p5(sketch);
+//variable sketch serves as template for what will run
+var sketch = function (p) {
+  p.setup = function () {
+    p.createCanvas(400, 400);
+  };
+
+  p.draw = function () {
+    p.noStroke();
+    p.background("#02f5f5");
+
+    //eyes
+
+    p.fill("#000000");
+    p.ellipse(150, 180, 60);
+    p.ellipse(250, 180, 60);
+  };
+};
+
+var myp5 = new p5(sketch);
+//variable sketch serves as template for what will run
+var sketch = function (p) {
+  p.setup = function () {
+    p.createCanvas(400, 400);
+  };
+
+  p.draw = function () {
+    p.noStroke();
+    p.background("#000000");
+
+    //eyes
+
+    p.fill("#02f5f5");
+    p.ellipse(150, 180, 60);
+    p.fill("#f0c7ee");
+    p.ellipse(250, 180, 60);
+  };
+};
+
+
